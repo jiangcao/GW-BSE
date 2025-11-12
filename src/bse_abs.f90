dbc7e06b64421c2a17d1f96511a650d752f5b3bb
Jiang Cao <jiang.cao89@gmail.com>
2025-11-12 13:33:15 +0100

    update

--- src/bse_abs.f90
+++ src/bse_abs.f90
@@ -20,14 +20,27 @@ complex(8)::Kernel_d,Kernel_x,tmp1
 logical:: lwcenter,lbse,lplotexciton
 character(8)  :: date
 character(10) :: time
+real(8) :: t_start, t_end, t_section_start
 
+! Print header and start time
+print '(/,80("="))'
+print '(a)', '        BSE Absorption Spectrum Calculation'
+print '(80("="),/)'
 call date_and_time(date,time)
-print '(a,2x,a)', date, time
+print '(a,a,1x,a)', 'Start time: ', date, time
+call cpu_time(t_start)
 
+! Read Hamiltonian data
+print '(/,a)', '>>> Loading Hamiltonian data...'
+call cpu_time(t_section_start)
 open(unit=10,file='ham_dat',status='unknown')
 call w90_load_from_file(10)
 close(10)
+call cpu_time(t_end)
+print '(a,f8.2,a)', '    Completed in ', t_end-t_section_start, ' seconds'
 
+! Read input parameters
+print '(/,a)', '>>> Reading input parameters...'
 open(unit=10,file='bse_input',status='unknown')
 read(10,*) NKX,NKY
 read(10,*) ncb,nvb
@@ -40,17 +53,38 @@ read(10,*) lbse
 read(10,*) ncpu
 read(10,*) e1(1),e1(2),e1(3)
 read(10,*) lplotexciton
-read(10,*) nex
-read(10,*) nx,ny,nz
-read(10,*) dx,dy,dz
+if (lplotexciton) then
+    read(10,*) nex
+    read(10,*) nx,ny,nz
+    read(10,*) dx,dy,dz
+endif
 close(10)
 
 call omp_set_num_threads(ncpu)
+call mkl_set_num_threads(ncpu)
 
 N=Nkx*Nky
 
-print '(3A15)','epsilon=','k0=','xi='
-print '(3F15.4)', epsilon,k0,xi
+print '(/,a)', '>>> Input parameters:'
+print '(a,i6,a,i6,a,i8,a)', '    k-grid: ', NKX, ' x ', NKY, ' (total ', N, ' k-points)'
+print '(a,i4,a,i4)', '    Bands: ncb = ', ncb, ', nvb = ', nvb
+print '(a,f12.6,a)', '    Electric field: F = ', F, ' V/Angstrom'
+print '(a,f10.4,a,f10.4,a,f10.4)', '    Screening: epsilon = ', epsilon, ', k0 = ', k0, ', xi = ', xi
+print '(a,f10.4,a)', '    Energy cutoff: ', E_cutoff, ' eV'
+print '(a,f10.6,a)', '    Gaussian broadening: ', sig, ' eV'
+print '(a,l2)', '    Use Wannier centers: ', lwcenter
+print '(a,l2)', '    Include BSE (e-h interaction): ', lbse
+print '(a,i4)', '    Number of CPU threads: ', ncpu
+print '(a,f8.4,a,f8.4,a,f8.4)', '    Light polarization: ', e1(1), ', ', e1(2), ', ', e1(3)
+if (lplotexciton) then
+    print '(a,i4)', '    Plot exciton wavefunction #', nex
+    print '(a,i4,a,i4,a,i4)', '    Grid: ', nx, ' x ', ny, ' x ', nz
+    print '(a,f8.4,a,f8.4,a,f8.4)', '    Grid spacing (Ang): ', dx, ', ', dy, ', ', dz
+endif
+
+! Diagonalize Hamiltonian on k-grid
+print '(/,a)', '>>> Diagonalizing Hamiltonian on k-grid...'
+call cpu_time(t_section_start)
 
 allocate(Hii(nb,nb))
 allocate(D(nb))
@@ -72,7 +106,11 @@ do i = 1,NKX
         ak(:,:,i,j) = Hii(:,:);           ! obtain eigen vectors corresponding to the eigen energies
     end do
 end do ! ek(band,kx,ky)
+call cpu_time(t_end)
+print '(a,f8.2,a)', '    Completed in ', t_end-t_section_start, ' seconds'
 
+! Write k-points and energies to file
+print '(/,a)', '>>> Writing k-points and band energies to files...'
 open(unit=11,file='kpoints.dat',status='unknown')
 open(unit=12,file='energies.dat',status='unknown')
 do i = 1,NKX
@@ -85,7 +123,12 @@ do i = 1,NKX
 end do    
 close(11)
 close(12)
+print '(a)', '    Files written: kpoints.dat, energies.dat'
 
+! Calculate interaction matrices in real space
+print '(/,a)', '>>> Calculating interaction matrices in real space...'
+print '(a)', '    Computing W (screened) and V (bare) potentials'
+call cpu_time(t_section_start)
 allocate(rij (3,NKX,NKY));             ! Rij=Ri-Rj
 allocate(rijvvd (3,nb,nb,NKX,NKY) );   ! Rijvvd= Ri-Rj+wannier center(iv)-wanniercenter(ivd)
 allocate(vrijvvd (nb,nb,NKX,NKY) );    ! bare potential 
@@ -96,7 +139,6 @@ r0=2*pi*xi
 xhat=(/1.0d0,0.0d0,0.0d0/)
 Length=norm(alpha)*(nkx);              !  Length of super cell along alpha direction (m)
 
-print *,'calculate the W and V matrices in real space'
 do i=0,NKX-1
   do j=0,NKY-1                
     minRv = dble(i)*alpha + dble(j)*beta    
@@ -123,6 +165,8 @@ do i=0,NKX-1
     end do                                    
   end do
 end do
+call cpu_time(t_end)
+print '(a,f8.2,a)', '    Completed in ', t_end-t_section_start, ' seconds'
 
 ! open(unit=11,file='wr.dat',status='unknown')
 ! open(unit=12,file='vr.dat',status='unknown')
@@ -143,7 +187,9 @@ end do
 ! close(13)
 
 if (lbse) then
-print *,'calculate fourier transform of W and V, wsum and vsum'
+print '(/,a)', '>>> Computing Fourier transform of W and V...'
+print '(a)', '    Calculating wsum (screened) and vsum (bare) in reciprocal space'
+call cpu_time(t_section_start)
 
 allocate(wsum(nb,nb,NKX*2,NKY*2));  ! sum of screened potentials
 allocate(vsum(nb,nb));              ! sum of bare potentials
@@ -169,6 +215,8 @@ do ikx=1,NKX*2
 end do
 !$omp end do
 !$omp end parallel
+call cpu_time(t_end)
+print '(a,f8.2,a)', '    Completed in ', t_end-t_section_start, ' seconds'
 end if
 
 do i=1,NKX
@@ -185,7 +233,9 @@ end do
 ! end do    
 ! close(11)
 
-print *,'build the BSE'
+! Build BSE Hamiltonian
+print '(/,a)', '>>> Building BSE Hamiltonian matrix...'
+call cpu_time(t_section_start)
 
 A=norm(cross(alpha,beta));          ! area of unit cell in Ang^2
 
@@ -208,8 +258,10 @@ do a1=1,nvb
 end do
 ind_keep = pack([(j,j=1,size(ediff))], reshape(ediff,(/nvb*ncb*NKX*NKY/)) .lt. E_cutoff) ! exclude values greater than Ecutoff
 newsize  = count(ind_keep .gt. 0)
-print *, 'nnz=', newsize
 num_cut = nvb*ncb*NKX*NKY - newsize
+print '(a,i10)', '    Total electron-hole pairs: ', nvb*ncb*NKX*NKY
+print '(a,i10,a,f6.2,a)', '    Kept after E_cutoff: ', newsize, ' (', 100.0*real(newsize)/real(nvb*ncb*NKX*NKY), '%)'
+print '(a,i10)', '    Excluded: ', num_cut
 allocate(index2(4,newsize))
 index2 = indice(:,ind_keep(1:newsize))
 deallocate(indice)
@@ -217,7 +269,9 @@ allocate(Knew(newsize,newsize) )
 knew = dcmplx(0.0d0,0.0d0)
 
 if (lbse) then       
-print *, '   calculate Kernel'
+print '(/,a)', '    Calculating BSE kernel (direct and exchange terms)...'
+print '(a,i10,a,i10)', '    Matrix size: ', newsize, ' x ', newsize
+call cpu_time(t_section_start)
 !$omp parallel default(none) private(i,ivb,icb,ikx,iky,j,jvb,jcb,jkx,jky,dk,iqx,iqy,Kernel_d,Kernel_x,v,vd) shared(newsize,index2,NKx,NKy,kv,q,ak,wsum,vsum,ediff,Knew,nb,vb0,totnvb)
 !$omp do
 do i = 1,newsize
@@ -241,9 +295,11 @@ do i = 1,newsize
         iqx = iqx+NKX;  ! Obtain the indices iqx and iqy where q=k-k'
         iqy = iqy+NKY;
         if (norm(dk(:) - q(:,iqx,iqy)) .gt. 1e-10)  then ! terminate if q#k-k'
-            print *, "k-k' ~= q !!"
-            print *,dk
-            print *,q(:,iqx,iqy)
+            print '(/,a)', 'ERROR: Momentum conservation violated!'
+            print '(a)', 'k - k'' does not match q in the grid'
+            print '(a,3f12.6)', 'dk     = ', dk
+            print '(a,3f12.6)', 'q      = ', q(:,iqx,iqy)
+            print '(a,e12.4)', 'difference = ', norm(dk(:) - q(:,iqx,iqy))
             stop
         end if
         !Implementations of equations 5 and 6 from the paper PRB 94, 245434
@@ -267,7 +323,10 @@ do i = 1,newsize
 end do
 !$omp end do
 !$omp end parallel
+call cpu_time(t_end)
+print '(a,f8.2,a)', '    Kernel calculation completed in ', t_end-t_section_start, ' seconds'
 else
+print '(/,a)', '    Skipping BSE kernel (lbse=F), using independent particle approximation'
 do i = 1,newsize
     
     ivb=index2(1,i)
@@ -279,11 +338,14 @@ do i = 1,newsize
 end do
 end if
 
+! Check Hermiticity
+print '(/,a)', '    Checking matrix Hermiticity...'
 if (any(abs(Knew - conjg(transpose(Knew))) .gt. 1e-7)) then
-    print *,'kernel not Hermitian!!'
-    print *, maxval(abs(Knew - conjg(transpose(Knew))))
+    print '(a)', '    ERROR: Kernel not Hermitian!'
+    print '(a,e12.4)', '    Maximum deviation: ', maxval(abs(Knew - conjg(transpose(Knew))))
     stop
 end if
+print '(a,e12.4,a)', '    Matrix is Hermitian (max deviation: ', maxval(abs(Knew - conjg(transpose(Knew)))), ')'
 
 if (lbse) then
 deallocate(wsum)
@@ -291,13 +353,20 @@ deallocate(vsum)
 deallocate(q)
 end if
 
-print *,'solve the BSE'
+! Solve BSE eigenvalue problem
+print '(/,a)', '>>> Solving BSE eigenvalue problem...'
+print '(a,i10,a,i10,a)', '    Diagonalizing ', newsize, ' x ', newsize, ' matrix'
+call cpu_time(t_section_start)
 
 allocate(omega(newsize))
 
 call mkl_set_num_threads(ncpu)
 
 omega=eigv(newsize,Knew)  ! solve the bse eigen value problem
+call cpu_time(t_end)
+print '(a,f8.2,a)', '    Diagonalization completed in ', t_end-t_section_start, ' seconds'
+print '(a,f10.4,a)', '    Lowest exciton energy: ', minval(omega), ' eV'
+print '(a,f10.4,a)', '    Highest exciton energy: ', maxval(omega), ' eV'
 
 allocate( asvckmat (nvb,ncb,NKX,NKY,newsize) )
 do s=1,newsize
@@ -313,6 +382,7 @@ do s=1,newsize
     end do
 end do
 
+print '(/,a)', '>>> Writing results to files...'
 open(unit=11,file='omega.dat',status='unknown')
 do s=1,newsize
     write(11,'(F15.4)') omega(s)    
@@ -332,8 +402,11 @@ do s=1,1
     enddo
 enddo
 close(11)
+print '(a)', '    Files written: omega.dat, asvck.dat'
 
-print *, 'compute the absorbance spectrum'
+! Compute absorption spectrum
+print '(/,a)', '>>> Computing absorption spectrum...'
+call cpu_time(t_section_start)
 
 allocate(sumas(newsize))
 sumas(:)=0.0d0
@@ -376,9 +449,16 @@ do s=1,newsize
     write(11,'(2F15.4)') omega(s) ,   dble(sumas(s))
 end do    
 close(11)
+call cpu_time(t_end)
+print '(a,f8.2,a)', '    Absorption spectrum computed in ', t_end-t_section_start, ' seconds'
+print '(a)', '    Files written: absp.dat, sumas.dat'
 
 if (lplotexciton) then
     
+    print '(/,a)', '>>> Computing exciton wavefunction...'
+    print '(a,i4)', '    Exciton state #', nex
+    print '(a,i4,a,i4,a,i4)', '    Grid size: ', nx, ' x ', ny, ' x ', nz
+    call cpu_time(t_section_start)
     npt=nx*ny*nz
     allocate(grid(3,npt))
     allocate(chi(nb,nb,nkx,nky))
@@ -394,11 +474,12 @@ if (lplotexciton) then
     enddo
     s=1
     
-    print *, 'compute the exciton wavefunction'
+    print '(a)', '    Computing exciton wavefunction in band basis...'
     call exciton_wavefunction_simple(asvckmat,s,NKX,NKY,ncb,nvb,newsize,nb,totnvb,ak,chi,kv,rij)
-    print *, 'compute the exciton wavefunction on the grid'    
+    print '(a)', '    Transforming to real-space grid...'    
     call exciton_wavefunction_grid(NKX,NKY,nb,chi,chixyz,rij,grid,npt,dz,wannier_center)        
     
+    print '(a)', '    Writing exciton wavefunction to files...'
     open(unit=11,file='chi_xy.dat',status='unknown')
     do i=1,nx
         do j=1,ny
@@ -414,37 +495,26 @@ if (lplotexciton) then
         end do
     end do
     close(11)
-    deallocate(grid)
-    deallocate(chi)
-    deallocate(chixyz)
+    call cpu_time(t_end)
+    print '(a,f8.2,a)', '    Exciton wavefunction computed in ', t_end-t_section_start, ' seconds'
+    print '(a)', '    Files written: chi_xy.dat, chi_xz.dat'
+    
 end if
 
-print *, 'Free memory'
-deallocate(Hii)
-deallocate(D)
-deallocate(Hiimat) 
-deallocate(ek)
-deallocate(ak)
-deallocate(kv)
-deallocate(rij)
-deallocate(rijvvd)
-deallocate(wrijvvd)
-deallocate(vrijvvd)
-deallocate(efieldpot)
-deallocate(ediff)
-deallocate(index2)
-deallocate(ind_keep)
-deallocate(Knew)
-deallocate(omega)
-deallocate(asvckmat)
-deallocate(sumas)
-deallocate(hbarw)
-deallocate(absp)
+! Clean up and print summary
+print '(/,a)', '>>> Cleaning up memory...'
 
 call w90_free_memory()
 
+! Print final summary
+call cpu_time(t_end)
+print '(/,80("="))'
+print '(a)', '        Calculation completed successfully!'
+print '(80("="))'
 call date_and_time(date,time)
-print '(a,2x,a)', date, time
+print '(a,a,1x,a)', 'End time:   ', date, time
+print '(a,f10.2,a)', 'Total time: ', t_end-t_start, ' seconds'
+print '(80("="),/)'
 
 END PROGRAM BSE_ABS
 
